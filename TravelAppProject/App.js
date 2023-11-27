5733081823062436309e63246f3419cdb15bcd39
Pham Dat <phamdat02012001@gmail.com>
2023-11-27 15:52:02 +0700

    Item screen

--- TravelAppProject/App.js
+++ TravelAppProject/App.js
@@ -16,7 +16,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Discover" component={Discover} />
-          {/* <Stack.Screen name="ItemScreen" component={ItemScreen} /> */}
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
